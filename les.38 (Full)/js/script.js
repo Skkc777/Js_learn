b7179516ae05da954817c39c98a5d542bfeb6d73
Stanislav <skkc@yandex.ru>
2020-07-22 09:37:10 +0300

    LESSON 38 animation class

--- les.38 (Full)/js/script.js
+++ les.38 (Full)/js/script.js
@@ -8,7 +8,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
     function tabContentHide() {
         tabsContent.forEach(item => {
-            item.style.display = 'none';
+            // item.style.display = 'none';
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
         });
 
         tabs.forEach(item => {
@@ -18,7 +20,9 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     function showTabContent(i = 0) {
-        tabsContent[i].style.display = 'block';
+        // tabsContent[i].style.display = 'block';
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
         tabs[i].classList.add('tabheader__item_active');
     }
 
